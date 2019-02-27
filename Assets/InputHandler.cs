c8bfddccb674bee4e8dd7b1ddd6b0145eba29a1b
Jesse Vander Does <jvanderdoes@gmail.com>
2019-02-27 09:47:40 -0800

    Example of issue with

---
+++ Assets/InputHandler.cs
@@ -0,0 +1,32 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour, IInputHandler
+{
+    public Material defaultMaterial;
+    public Material clickMaterial;
+
+    public void OnInputDown(InputEventData eventData)
+    {
+        Debug.Log("InputHandler.OnInputDown");
+        GetComponent<Renderer>().material = clickMaterial;
+    }
+
+    public void OnInputUp(InputEventData eventData)
+    {
+        Debug.Log("InputHandler.OnInputUp");
+        GetComponent<Renderer>().material = defaultMaterial;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
