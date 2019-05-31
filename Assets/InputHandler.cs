08312d9c3de249377dc6a3c12397eedc1b6dd8fd
Jesse Vander Does <jvanderdoes@gmail.com>
2019-05-31 09:49:30 -0700

    Reproduces bug that prevents world anchor from being deleted after HL is woken from sleep

--- Assets/InputHandler.cs
+++ Assets/InputHandler.cs
@@ -1,17 +1,29 @@
-﻿using HoloToolkit.Unity.InputModule;
+﻿using HoloToolkit.Unity;
+using HoloToolkit.Unity.InputModule;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InputHandler : MonoBehaviour, IInputHandler
 {
-    public Material defaultMaterial;
+    public TextMesh textMesh;
+    public bool clicked = false;
     public Material clickMaterial;
+    public Material defaultMaterial;
 
     public void OnInputDown(InputEventData eventData)
     {
-        Debug.Log("InputHandler.OnInputDown");
         GetComponent<Renderer>().material = clickMaterial;
+        if (clicked)
+        {
+            WorldAnchorManager.Instance.AttachAnchor(gameObject, "AnchorName");
+        }
+        else
+        {
+            WorldAnchorManager.Instance.RemoveAnchor("AnchorName");
+        }
+        clicked = clicked == false;
     }
 
     public void OnInputUp(InputEventData eventData)
@@ -22,11 +34,11 @@ public class InputHandler : MonoBehaviour, IInputHandler
 
     // Use this for initialization
     void Start () {
-		
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        string[] ids = WorldAnchorManager.Instance.AnchorStore.GetAllIds();
+        textMesh.text = "Anchor IDs: " + String.Join(",", ids);
+    }
 }
