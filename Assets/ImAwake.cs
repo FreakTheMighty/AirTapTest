78e1d93859a6d137aeedb47f0fd4018a9472f342
Jesse Vander Does <jvanderdoes@gmail.com>
2019-06-13 12:16:31 -0700

    Branch demonstrating voice commands failing after hl sleeps

---
+++ Assets/ImAwake.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImAwake : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateCube()
+    {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = Camera.main.transform.position + (Camera.main.transform.forward * 8);
+        cube.transform.rotation = Random.rotation;
+        Destroy(cube, 3);
+    }
+}
